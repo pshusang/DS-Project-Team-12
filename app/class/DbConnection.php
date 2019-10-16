814d550824144acdb24261f1931f143727b862ef
pshusang <38292263+pshusang@users.noreply.github.com>
2019-10-16 11:24:44 -0400

    Fix Dbconnection

--- app/class/DbConnection.php
+++ app/class/DbConnection.php
@@ -14,12 +14,12 @@ class DbConnection
       }
 
       try {
-          $dsn = 'mysql:host='.getenv('team12ds.cs5bo2vvf8tr.us-east-1.rds.amazonaws.com').';dbname='.getenv('team12ds').';charset=utf8');
+          $dsn = 'mysql:host='.getenv('MYSQL_HOST').';dbname='.getenv('MYSQL_DATABASE').';charset=utf8');
           error_log($dsn);
           self::$connection = new PDO(
              $dsn,
-             getenv('team12'),
-             getenv('muesli12'),
+             getenv('MYSQL_USER'),
+             getenv('MYSQL_PASSWORD'),
              [
                  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
