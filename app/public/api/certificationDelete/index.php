af066087b1f2cca8af4c4971132fa8a67bbadf32
mehtac <mehtac@iu.edu>
2019-10-27 11:43:09 -0400

    certifications typo

--- app/public/api/certificationDelete/index.php
+++ app/public/api/certificationDelete/index.php
@@ -11,7 +11,7 @@ $stmt->execute();
 $members = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($members, JSON_PRETTY_PRINT);
+$json = json_encode($certifications, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
